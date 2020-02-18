31ad5b9834e4392003810aa5d13c94a623477312
Jonathan Helsing <jonathanhelsing.source@gmail.com>
2020-02-18 23:31:48 +0100

    server.c file created. The files has a layout of 5 steps to follow to complete the task.

---
+++ server.c
@@ -0,0 +1,21 @@
+/*===========================================================================================================
+		This is a simple server in the internet domain using TCP, the port number is passed as an argument.
+============================================================================================================/*
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+int main(int argc, char *argv[])
+{
+	
+    // Create a socket using socket().
+    // Bind the socket to an address using bind().
+    // Listen for connections using listen().
+    // Accept a connection using accept().
+    // Send and receive data using send() and recv().
+
+
+	EXIT_SUCCESS;
+}
