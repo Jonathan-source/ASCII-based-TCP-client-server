178e58a406c3fcdf02a53e63083ffd3e41d347b5
Jonathan Helsing <jonathanhelsing.source@gmail.com>
2020-02-19 11:06:30 +0100

    some modifications

--- server.c
+++ server.c
@@ -1,5 +1,5 @@
 /*===========================================================================================================
-	This is a simple server in the internet domain using TCP, the port number is passed as an argument.
+								A simple TCP server on port number 9002.
 ============================================================================================================/*
 
 #include <stdio.h>			
@@ -11,12 +11,15 @@ int main(int argc, char *argv[])
 {
 	int sockfd;
 	int portno;
-	sockaddr_in server_addr;
-	sockaddr_in client_addr;
+	int clientsocket;
+	char buffer[256];
+	sockaddr_in server;
+	sockaddr_in client;
+	socklen_t clientSize;
 	
     //============== 1. Create a socket using socket(). ==============================
 
-    	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	if(sockfd < 0)
 	{
 		std::cout << "[Error]: Socket not created.";
@@ -25,15 +28,15 @@ int main(int argc, char *argv[])
 	
 	//============== 2. Bind the socket to a socket address using bind(). ============
 
-	portno = atoi(argv[1]);											// Store the port number
-	bzero((char*) &server_addr, sizeof(server_addr));  				// Clear the server address before using it.
+	portno = 9002													// Set port number
+	bzero((char*) &server, sizeof(server));  						// Clear the server address before using it.
 
 	// Socket address information needed for binding.
-	server_addr.sin_family = AF_INET;
-	server_addr.sin_port = htons(portno);							
-	server_addr.sin_addr.s_addr = INADDR_ANY; 						// "0.0.0.0" 
-	
-	if(bind(sockfd, (struct sockaddr * ) &server_addr, sizeof(server_addr)) < 0)
+	server.sin_family = AF_INET;
+	server.sin_port = htons(portno);							
+	server.sin_addr.s_addr = INADDR_ANY; 							// "0.0.0.0" 
+
+	if(bind(sockfd, (struct sockaddr * ) &server, sizeof(server)) < 0)
 	{
 		std::cout "[Error]: Failed to bind socket to a socket address.";
 		return EXIT_FAILURE;
@@ -41,15 +44,32 @@ int main(int argc, char *argv[])
 
 
     //============== 3. Listen for connections using listen(). =======================
-
+	
+	// Mark the socket for listening.
+	if(listen(sockfd, SOMAXCONN) < 0)								// Backlog is the maximum number of queued connections, 
+	{																// you want on a socket. SOMAXCONN is the max number.
+		std::cout "[Error]: Listen failed.";
+	}
 
 
     //============== 4. Accept a connection using accept(). ==========================
+	clientSize = sizeof(client);
+		
+	if(accept(sockfd, (sockaddr*) &client), &clientSize))
+	{
+		std::cout << "[Error]: Could not connect with the client."
+		return EXIT_FAILURE;
+	}
+
+	// Close the listenning socket.
 
 
 
-    //============== 5. Send and receive data using send() and recv(). ===============
+	//============== 5. Send and receive data using send() and recv(). ===============
 
+
+
+	// Close socket.
 
 	EXIT_SUCCESS;
 }
