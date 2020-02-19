043f3005368c5eac64f7f206548cfaf373cd1a4e
Jonathan Helsing <jonathanhelsing.source@gmail.com>
2020-02-19 10:34:25 +0100

    socket() & add() steps done

--- server.c
+++ server.c
@@ -9,10 +9,14 @@
 
 int main(int argc, char *argv[])
 {
+	int sockfd;
+	int portno;
+	sockaddr_in server_addr;
+	sockaddr_in client_addr;
 	
     //============== 1. Create a socket using socket(). ==============================
 
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	if(sockfd < 0)
 	{
 		std::cout << "[Error]: Socket not created.";
@@ -21,13 +25,13 @@ int main(int argc, char *argv[])
 	
 	//============== 2. Bind the socket to a socket address using bind(). ============
 
-	// Socket address information needed for binding.
-	sockaddr_in server_addr, client_addr;
-	server_addr.sin_family = AF_INET;
-	server_addr.sin_port = htons(9009);								// Temporary random high number that is not used by the OS. It will later be passed as an argument.
-	server_addr.sin_addr.s_addr = INADDR_ANY; 						// "0.0.0.0" 
-
+	portno = atoi(argv[1]);											// Store the port number
 	bzero((char*) &server_addr, sizeof(server_addr));  				// Clear the server address before using it.
+
+	// Socket address information needed for binding.
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(portno);							
+	server_addr.sin_addr.s_addr = INADDR_ANY; 						// "0.0.0.0" 
 	
 	if(bind(sockfd, (struct sockaddr * ) &server_addr, sizeof(server_addr)) < 0)
 	{
