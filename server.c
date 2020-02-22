37c538474769a99ff2ee8474dcacf86a9268d88a
Jonathan Helsing <jonathanhelsing.source@gmail.com>
2020-02-22 19:49:54 +0100

    code updated

--- server.c
+++ server.c
@@ -27,14 +27,14 @@ int main(int argc, char *argv[])
 	
 //============== 1. Create a socket using socket(). ================================
 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-
-	if(sockfd < 0)
-		perror("socket was not created.");
+    if(sockfd = socket(AF_INET, SOCK_STREAM, 0) < 0)
+		perror("[Server]: socket was not created.");
+	else
+		printf("[Server]: socket was created.");
 	
 //============== 2. Bind the socket to a socket address using bind(). =============
 											
-	memset(&serverAddr, 0, sizeof(serverAddr));  							// Zero out structure.
+	memset(&serverAddr, 0, sizeof(serverAddr));  								// Zero out structure.
 
 	// Socket address information needed for binding.
 	serverAddr.sin_family = AF_INET;
@@ -42,38 +42,40 @@ int main(int argc, char *argv[])
 	serverAddr.sin_addr.s_addr = INADDR_ANY; 									// "0.0.0.0" 
 
 	if(bind(sockfd, (struct sockaddr * ) &serverAddr, sizeof(serverAddr)) < 0)
-		perror("socket was not bound to a socket address.");
+		perror("[Server]: socket was not bound to a socket address.");
+	else
+		printf("[Server]: socket was bound to socket address.");
 
 //============== 3. Listen for connections using listen(). ========================
 	
-	// Mark the socket for listening.
-
 	if(listen(sockfd, SOMAXCONN) < 0)	
-		perror("listen failed.");
-	printf("waiting for a connection on port: %i", SERVER_PORT);
+		perror("[Server]: listen was not performed.");
+	else
+		printf("[Server]: waiting for a connection on port: %i", SERVER_PORT);
 
 //============== 4. Accept a connection using accept(). ===========================
 
 	clientLen = sizeof(clientAddr);
 		
 	if((connfd = accept(sockfd, (struct sockaddr *) &clientAddr, &clientLen)) < 0)
-		perror("could not connect with the client.");
+		perror("[Server]: could not connect with the client.");
 
 //============== 5. Send and receive data using send() and recv(). ================
 
 	// Receive data.
-	memset(recvBuffer, '\n', sizeof(recvBuffer));
-	if(recv(connfd, buffer, sizeof(recvBuffer), 0) < 0)
-		perror("could not read from socket.");
-	printf("\n%s", recvBuffer);
+	memset(recvBuff, '\n', sizeof(recvBuff));
+	if(recv(connfd, recvBuff, sizeof(recvBuff), 0) < 0)
+		perror("[Server]: could not read from socket.");
+	else
+		printf("\n%s", recvBuff);
 
 	// Send data.
-	memset(sendBuffer, '\n', sizeof(sendBuffer));
-	sendBuffer[256] = "Data reseived.";
-	if(send(connfd, sendBuffer, 15, 0) < 0)
-		perror("could not write to socket.");
+	memset(sendBuff, '\n', sizeof(sendBuff));
+	if(send(connfd, "[Server]: data received.", 15, 0) < 0)
+		perror("[Server]: could not write to socket.");
 
 	// Close socket.
-	close(sockfd);
+	// ???
+	
 	return 0;
 }
