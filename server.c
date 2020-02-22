0c13dee4ffd6e48c30ad9ce110a32be5f9d8e531
Jonathan Helsing <jonathanhelsing.source@gmail.com>
2020-02-22 17:24:53 +0100

    code updated, server completed

--- server.c
+++ server.c
@@ -1,19 +1,13 @@
 /*===========================================================================================================
 				A simple TCP server on port number 9002.
-============================================================================================================/*
+============================================================================================================*/
 
-#include <stdio.h>			
+#include <stdio.h>				
 #include <sys/types.h> 		
 #include <sys/socket.h>		 
 #include <netinet/in.h>	
-#include <string>
-#include <iostream>	 
 
- void error(std::string &msg)
- {
-	std::cout << "[Error]: " + msg;
-	return EXIT_FAILURE;
- }
+void perror(char const * msg);
 
 int main(int argc, char *argv[])
 {
@@ -30,12 +24,12 @@ int main(int argc, char *argv[])
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
 	if(sockfd < 0)
-		error("socket was not created.");
+		perror("socket was not created.");
 	
 //============== 2. Bind the socket to a socket address using bind(). =============
 
 	portno = 9002													
-	bzero((char*) &server, sizeof(server));  					// Clear the server address before using it.
+	memset(&server, sizeof(server));  							// Zero out structure.
 
 	// Socket address information needed for binding.
 	server.sin_family = AF_INET;
@@ -43,31 +37,36 @@ int main(int argc, char *argv[])
 	server.sin_addr.s_addr = INADDR_ANY; 						// "0.0.0.0" 
 
 	if(bind(sockfd, (struct sockaddr * ) &server, sizeof(server)) < 0)
-		error("socket was not bound to a socket address.");
+		perror("socket was not bound to a socket address.");
 
 //============== 3. Listen for connections using listen(). ========================
 	
 	// Mark the socket for listening.
 
 	if(listen(sockfd, SOMAXCONN) < 0)	
-		error("listen failed.");
+		perror("listen failed.");
 
 //============== 4. Accept a connection using accept(). ===========================
+
 	clientSize = sizeof(client);
 		
-	if(accept(sockfd, (sockaddr*) &client), &clientSize))
-		error("could not connect with the client.");
-
-
-	// Close the listenning socket.
-	
-
+	if(accept(sockfd, (sockaddr*) &client), &clientSize);
+		perror("could not connect with the client.");
 
 //============== 5. Send and receive data using send() and recv(). ================
 
+	// Receive / read data.
+	memset(buffer, 256);
+	if(recv(sockfd, buffer, 256) < 0)
+		perror("could not read from socket.");
+	printf("%s", buffer);
+
+	// Send string to the server.
+	if(send(sockfd, "Success", 7) < 0)
+		perror("could not write to socket.");
 
 
 	// Close socket.
-
-	EXIT_SUCCESS;
+	close(sockfd);
+	return 0;
 }
